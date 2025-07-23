0c9e3e49517feeb7488516b3eaa6cb1c8ff0142b
Kyle Kelley <rgbkrk@gmail.com>
2025-07-22 20:24:28 -0700

    more simple tests in schema package

--- packages/schema/test.ts
+++ packages/schema/test.ts
@@ -23,17 +23,81 @@ async function setupStore() {
     // sync: { backend: makeCfSync({ url: '...' }) },
   });
 
-  const store = await createStorePromise({ adapter, schema, storeId: "test" });
+  const store = await createStorePromise({
+    adapter,
+    schema,
+    storeId: "test",
+    onBootStatus: (status) => {
+      console.table({
+        status,
+      });
+    },
+    otelOptions: {
+      //   serviceName: "test",
+      //   serviceVersion: "0.0.1",
+    },
+    disableDevtools: true,
+  });
 
   return store;
 }
 
 Deno.test("simple test", async () => {
   const store = await setupStore();
-
   const cells = store.query(tables.cells);
 
   assertEquals(cells.length, 0);
+
+  store.commit(events.cellCreated({
+    id: "1",
+    createdBy: "deno",
+    cellType: "code",
+    position: 0,
+  }));
+
+  assertEquals(store.query(tables.cells).length, 1);
+
+  store.commit(events.cellSourceChanged({
+    id: "1",
+    source: "print('Hello, world!')",
+    modifiedBy: "deno",
+  }));
+
+  assertEquals(store.query(tables.cells).length, 1);
+  assertEquals(
+    store.query(tables.cells.select().where({ id: "1" }))[0].source,
+    "print('Hello, world!')",
+  );
+
+  store.shutdown();
+});
+
+Deno.test("simple test", async () => {
+  const store = await setupStore();
+  const cells = store.query(tables.cells);
+
+  assertEquals(cells.length, 0);
+
+  store.commit(events.cellCreated({
+    id: "1",
+    createdBy: "deno",
+    cellType: "ai",
+    position: 0,
+  }));
+
+  assertEquals(store.query(tables.cells).length, 1);
+
+  store.commit(events.cellSourceChanged({
+    id: "1",
+    source: "Create some cells",
+    modifiedBy: "deno",
+  }));
+
+  assertEquals(store.query(tables.cells).length, 1);
+  assertEquals(
+    store.query(tables.cells.select().where({ id: "1" }))[0].source,
+    "Create some cells",
+  );
 
   store.shutdown();
 });
