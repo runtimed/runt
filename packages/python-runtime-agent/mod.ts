1e69340617803234215ae68514eb21c474907cbe
Kyle Kelley <rgbkrk@gmail.com>
2025-08-25 08:38:10 -0700

    move runner bits into pyodide runtime agent

--- packages/python-runtime-agent/mod.ts
+++ packages/python-runtime-agent/mod.ts
@@ -1,8 +1,35 @@
 import { PythonRuntimeAgent } from "./src/python-runtime-agent.ts";
-import { runner } from "@runt/lib";
+import { createLogger } from "@runt/lib";
 export { PythonRuntimeAgent };
 
 if (import.meta.main) {
+  const name = "PyRunt";
+
+  const logger = createLogger(name);
   const agent = new PythonRuntimeAgent();
-  await runner(agent, "PyRunt");
+
+  logger.info("Starting Agent");
+
+  try {
+    await agent.start();
+
+    logger.info(`${name} started`, {
+      runtimeId: agent.config.runtimeId,
+      runtimeType: agent.config.runtimeType,
+      notebookId: agent.config.notebookId,
+      sessionId: agent.config.sessionId,
+      syncUrl: agent.config.syncUrl,
+    });
+
+    await agent.keepAlive();
+  } catch (error) {
+    logger.error(`${name} failed to start`, error);
+    Deno.exit(1);
+  }
+
+  agent.shutdown().catch((error) => {
+    logger.error(`${name} failed to shutdown`, error);
+    Deno.exit(1);
+  });
+  Deno.exit(0);
 }
