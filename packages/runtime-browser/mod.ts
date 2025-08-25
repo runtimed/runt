7cd588650b917f480f703efcc7b0f65eb7434088
Kyle Kelley <rgbkrk@gmail.com>
2025-08-25 07:44:44 -0700

    Add stub packages for runtime refactor
    - Create @runt/runtime-core package (v0.0.1)
    - Create @runt/runtime-browser package (v0.0.1)
    - Create @runt/runtime-deno package (v0.0.1)
    - Add packages to workspace and publish config
    - All packages have TBD placeholders for future refactoring

---
+++ packages/runtime-browser/mod.ts
@@ -0,0 +1,13 @@
+/**
+ * @runt/runtime-browser - Browser runtime for runt agents
+ *
+ * TODO: Extract browser-specific runtime functionality from @runt/lib
+ */
+
+// Placeholder export until functionality is moved from @runt/lib
+export const TBD = "TBD" as const;
+
+export type RuntimeBrowser = {
+  // TODO: Define browser runtime interfaces
+  placeholder: typeof TBD;
+};
